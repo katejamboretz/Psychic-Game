55b2af5d14da96498b3d55d8e2b618457391e078
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2020-02-18 21:52:10 -0800

    correct variable declaration and write initial game data to html from javascript, link jquery library

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,61 @@
-// Declare variables
-
-winTot = 0;
-lossTot = 0;
-guessLeft = 9;
-guessArray = [];
-alphArray = [
-  a,
-  b,
-  c,
-  d,
-  e,
-  f,
-  g,
-  h,
-  i,
-  j,
-  k,
-  l,
-  m,
-  n,
-  o,
-  p,
-  q,
-  r,
-  s,
-  t,
-  u,
-  v,
-  w,
-  x,
-  y,
-  z
-];
-userGuess;
-compGuess;
-
 // When document ready
+$(document).ready(function() {
+  // Declare variables
 
-// Display data in html tags
+  var winTot = 0;
+  var lossTot = 0;
+  var guessLeft = 9;
+  var guessArray = [];
+  var alphArray = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+  var userGuess;
+  var compGuess;
 
-// Create on key events for all letters and store in userGuess
+  // Display data in html tags
 
-// Write letters to guessArray with each on key event
+  $("#wins").text("Wins: " + winTot);
+  $("#losses").text("Losses: " + lossTot);
+  $("#guess-left").text("Guesses Left: " + guessLeft);
 
-// Subtract 1 from guessLeft with each on key event
+  // Create on key events for all letters and store in userGuess
 
-// Make function to use random number generator / round down and apply to alphArray to generate compGuess
+  // Write letters to guessArray with each on key event
 
-// Create reset function to reset guessArray and guessLeft and generate compGuess
+  // Subtract 1 from guessLeft with each on key event
 
-// write if or case statements
+  // Make function to use random number generator / round down and apply to alphArray to generate compGuess
 
-// if compGuess the same as userGuess, reset, add 1 to wins
+  // Create reset function to reset guessArray and guessLeft and generate compGuess
 
-// else guessLeft = 0, reset, add 1 to losses,
+  // write if or case statements
+
+  // if compGuess the same as userGuess, reset, add 1 to wins
+
+  // else guessLeft = 0, reset, add 1 to losses,
+});
