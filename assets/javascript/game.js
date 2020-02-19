806a49fa17c2b9f7b1ee1397f2a648deb2223456
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2020-02-18 22:35:44 -0800

    add functionality to all letters to write to guess-left id and subtract from guess total, by copy and paste

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -45,11 +45,239 @@ $(document).ready(function() {
 
   // Create on key events for all letters and store in userGuess
   // Refer to https://codegazerants.com/2015/09/12/javascript-catch-keystrokes/
+  // Use https://keycode.info to know what ID goes with what keystroke
+  // notice letter keys go from 65-90
   document.onkeydown = function(userGuess) {
     switch (userGuess.keyCode) {
       case 65:
         var let = "a";
-        guessArray.push("" + userGuess + "");
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 66:
+        var let = "b";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 67:
+        var let = "c";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 68:
+        var let = "d";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 69:
+        var let = "e";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 70:
+        var let = "f";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 71:
+        var let = "g";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 72:
+        var let = "h";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 73:
+        var let = "i";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 74:
+        var let = "j";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 75:
+        var let = "k";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 76:
+        var let = "l";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 77:
+        var let = "m";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 78:
+        var let = "n";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 79:
+        var let = "o";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 80:
+        var let = "p";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 81:
+        var let = "q";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 82:
+        var let = "r";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 83:
+        var let = "s";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 84:
+        var let = "t";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 85:
+        var let = "u";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 86:
+        var let = "v";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 87:
+        var let = "w";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 88:
+        var let = "x";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 89:
+        var let = "y";
+        guessArray.push(let);
+        console.log(guessArray);
+        $("#guess-array").append("<p>" + let + "</p>");
+        guessLeft--;
+        $("#guess-left").text("Guesses Left: " + guessLeft);
+        break;
+
+      case 90:
+        var let = "z";
+        guessArray.push(let);
+        console.log(guessArray);
         $("#guess-array").append("<p>" + let + "</p>");
         guessLeft--;
         $("#guess-left").text("Guesses Left: " + guessLeft);
