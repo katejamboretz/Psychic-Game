87689ecb9afb4d8a58b906f6bd9da87bb558f172
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2020-02-18 21:32:40 -0800

    add variables and pseudocode strategy into game.js. update ids in html

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+// Declare variables
+
+winTot = 0;
+lossTot = 0;
+guessLeft = 9;
+guessArray = [];
+alphArray = [
+  a,
+  b,
+  c,
+  d,
+  e,
+  f,
+  g,
+  h,
+  i,
+  j,
+  k,
+  l,
+  m,
+  n,
+  o,
+  p,
+  q,
+  r,
+  s,
+  t,
+  u,
+  v,
+  w,
+  x,
+  y,
+  z
+];
+userGuess;
+compGuess;
+
+// When document ready
+
+// Display data in html tags
+
+// Create on key events for all letters and store in userGuess
+
+// Write letters to guessArray with each on key event
+
+// Subtract 1 from guessLeft with each on key event
+
+// Make function to use random number generator / round down and apply to alphArray to generate compGuess
+
+// Create reset function to reset guessArray and guessLeft and generate compGuess
+
+// write if or case statements
+
+// if compGuess the same as userGuess, reset, add 1 to wins
+
+// else guessLeft = 0, reset, add 1 to losses,
